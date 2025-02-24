8431445be21f4f1ac80e70dc7b67a72bb597e97b
Jyri Hakala <jyri.hakala@tuni.fi>
2025-02-24 17:36:35 +0200

    Set connection configuration

--- backend/src/main/java/com/github/dreamteam/MongoClientConfig.java
+++ backend/src/main/java/com/github/dreamteam/MongoClientConfig.java
@@ -2,14 +2,38 @@ package com.github.dreamteam;
 
 // TODO: imports
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ConnectionString;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoClientConfig extends AbstractMongoClientConfiguration {
 
-    // TODO: implement methods
+    private static final Logger LOGGER = LoggerFactory.getLogger(MongoClientConfig.class);
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Value("${spring.data.mongodb.uri}")
+    private String connectionString;
+
     @Override
     protected String getDatabaseName() {
-        return "FIXME";
+        return databaseName;
+    }
+
+    @Bean
+    public MongoClientSettings mongoClientSettings() {
+        LOGGER.info("Connecting to MongoDB at {}", connectionString);
+        return MongoClientSettings.builder()
+            .applyConnectionString(new ConnectionString(connectionString))
+            .build();
     }
 }
