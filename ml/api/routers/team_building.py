7a6bfe9bd31d413f1d32b5976875682c91f4fe28
Toni Grönberg <togro@hotmail.fi>
2025-02-25 18:31:02 +0200

    Complete teamBuilder API

--- ml/api/routers/team_building.py
+++ ml/api/routers/team_building.py
@@ -8,31 +8,64 @@ router = APIRouter()
 def build_team(
     projectId: int = Query(description="ID of the project to fetch scores for"),
     size: int = Query(default=5, description="Number of team members"),
-    data: str = Query(default="student_score_example", description="Name of the score file")
+    data: str = Query(default="score_v1", description="Name of the score file"),
+    saveFile: str = Query(default="team_v1", description="Name of the file team is saved")
 ):
     """
     Builds the team based on stored predictions/scores
 
      Args:
         projectId (int): id for project
-        size (ing): size of the team, default=5
+        size (int): size of the team, default=5
+        data (string): Name of the score file
+        saveFile (String): Name of the file team is saved
 
     Returns:
         dict/json response: A list of team members.
+
+
+    Expected return format:
+
+    {
+        "projectId": 1046,
+        "team": 
+        [
+            {
+                "studentId": 21975,
+                "Score": 100.0
+            },
+            {
+                "studentId": 22044,
+                "Score": 100.0
+            }
+        ]
+    }
+
     """
     #print(f"Received projectId: {projectId}")
 
+    """n projectId loadName saveName"""
+
     try:
-        team_data = team_builder.build_team(size, data)
-        if not team_data["team"]:
+        team_data = team_builder.build_team(size, projectId, data, saveFile)
+
+        if not team_data.get("team"):
             return JSONResponse(
             status_code = 400,
             content={"error": "No valid team members found. Check data"}
             )
+        
+        #Extract only studentId and Score
+        filtered_team = [
+            {"studentId": member["studentId"], "Score": member["Score"]}
+            for member in team_data["team"]
+        ]
+
         return JSONResponse(
             status_code=200,
-            content={"projectId": projectId, "team":team_data["team"]}
+            content={"projectId": projectId, "team": filtered_team}
         )
+    
     except Exception as e:
         return JSONResponse(
             status_code=500,
