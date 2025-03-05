f5da65a93ea6bdcc6d2eb63f5ee6549cfd90b36a
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2025-03-05 16:18:57 +0200

    See prev commit (f961da2c460c9fbeb2325740420d1122ae2fb03c)

--- frontend/client/src/services/project/project.service.ts
+++ frontend/client/src/services/project/project.service.ts
@@ -3,22 +3,8 @@ import { Project } from "../../types/Project";
 
 /* Components, services & etc. */
 import { callAPI, USE_SERVER } from "../api/api.service";
-
-const testProjects: Array<Project> = [
-    {
-        id: 1,
-        name: "test 1",
-    },
-    {
-        id: 2,
-        name: "test 2",
-    },
-    {
-        id: 3,
-        name: "test 3",
-    }
-];
+import { defaultProjects } from "./default-projects";
 
 export const getProjects = async (): Promise<Project[]> => {
-    return USE_SERVER ? callAPI<Project[]>("/projects") : Promise.resolve(testProjects);
+    return USE_SERVER ? callAPI<Project[]>("/projects") : Promise.resolve(defaultProjects);
 }
