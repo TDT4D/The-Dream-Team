5b237ebc95a621bd7c553bf0ddfb26a1fae8ddf9
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2025-03-12 00:08:43 +0200

    Created working student status labels
    The labels (selected & applied) are stored for each student in the
    browser's localstorage. As the program is just for demo purposes I think
    this should be enough atm.
    
    Also the applied projects are only added once
    the given project is selected.

--- frontend/client/src/services/student/student.service.ts
+++ frontend/client/src/services/student/student.service.ts
@@ -1,12 +1,14 @@
 /* Types */
 import { AuthToken } from "../../types/Auth";
 import { ColumnType } from "../../types/Columns";
+import { Label, LabelContent, LabelType } from "../../types/Label";
 import { Project } from "../../types/Project";
 import { Student } from "../../types/Student";
 
 /* Components, services & etc. */
 import { callAPI, USE_SERVER } from "../api/api.service";
 import { defaultStudents } from "./default-students";
+import storage from "../storage/storage.service";
 
 // TODO: Get stuff from local storage
 export const checkStudentStatus = (student: Student): ColumnType => {
@@ -32,4 +34,22 @@ export const getStudent = (id: number): Student => {
 
 export const getStudents = (projectID: Project["id"], token: AuthToken): Promise<Student[]> => {
     return USE_SERVER ? callAPI<Student[]>(`/projects/${projectID}/students`, token) : Promise.resolve(defaultStudents);
+}
+
+const storageLabelsID = (studentId: Student["id"], labelType: LabelType): string => `s${studentId}l${labelType}`;
+
+export const getStudentLabels = (studentId: Student["id"], labelType: LabelType): LabelContent[] => {
+    const saved = storage.get<LabelContent[]>(storageLabelsID(studentId, labelType));
+    return saved ?? [];
+}
+
+export const addLabelIfMissing = (studentId: Student["id"], label: Label): void => {
+    const mapper = (old: LabelContent[]) => [...old.filter(lbl => lbl.content !== label.contains.content), label.contains];
+    if (!storage.update<LabelContent[]>(storageLabelsID(studentId, label.isType), mapper)) {
+        storage.save<LabelContent[]>([label.contains], storageLabelsID(studentId, label.isType));
+    }
+}
+
+export const removeLabelFromStudent = (studentId: Student["id"], label: Label): void => {
+    storage.update<LabelContent[]>(storageLabelsID(studentId, label.isType), (old: LabelContent[]) => old.filter(c => c.content !== label.contains.content));
 }
